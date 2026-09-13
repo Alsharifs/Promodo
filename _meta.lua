f88dccde8bd4d5a015e155cb835cec068f00e3bc
Sayed Moustafa <72506048+Alsharifs@users.noreply.github.com>
2026-09-13 05:01:02 +0400

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,8 +0,0 @@
-local _ = require("gettext")
-
-return {
-    name = "advanced_promodo",
-    fullname = _("Advanced Pomodoro Timer"),
-    description = _("A standalone Pomodoro timer for KOReader."),
-    version = "1.0.0",
-}
